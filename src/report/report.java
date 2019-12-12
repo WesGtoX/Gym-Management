9c8565c007f8674a59e22783427a2062b7ed24a2
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2019-12-12 02:20:21 -0300

    Fix xls reports export

--- src/report/report.java
+++ src/report/report.java
@@ -9,7 +9,8 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.design.JasperDesign;
-import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporterParameter;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
 import net.sf.jasperreports.view.JasperViewer;
 import static view.FormReports.exp;
@@ -23,7 +24,9 @@ public class report {
         
     //Display paremeterized report
     public void display(Connection con, String report,HashMap options){
+        
         try {
+            
             //Load report layout
             JasperDesign jd = JRXmlLoader.load(getClass().getResourceAsStream("/report/"+report));
             
@@ -39,25 +42,24 @@ public class report {
             
             if(exp.equals("PDF")){
                 //Export PDF report
-                JasperExportManager.exportReportToPdfFile(jp,"c:\\relatorio.pdf");
-            }
-            
-            if(exp.equals("HTML")){
-                //Export HTML report
-                JasperExportManager.exportReportToHtmlFile(jp,"c:\\relatorio.pdf");
+                JasperExportManager.exportReportToPdfFile(jp,System.getProperty("user.home") + "\\Desktop\\Relatório.pdf");
             }
             
             if(exp.equals("XLSx")){
-                //Export XLSx report
-                JRXlsxExporter export = new JRXlsxExporter();
-                export.setParameter(JRExporterParameter.JASPER_PRINT, jp);
-                export.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, report);
-                export.exportReport();
+                //Export XLS report
+                JRXlsExporter xlsReport = new JRXlsExporter();
+                xlsReport.setParameter(JRXlsExporterParameter.JASPER_PRINT, jr);
+                xlsReport.setParameter(JRExporterParameter.OUTPUT_FILE_NAME, System.getProperty("user.home") + "\\Desktop\\Relatório.xls");
+                xlsReport.setParameter(JRXlsExporterParameter.IS_REMOVE_EMPTY_SPACE_BETWEEN_ROWS,Boolean.TRUE);
+                xlsReport.setParameter(JRXlsExporterParameter.IS_WHITE_PAGE_BACKGROUND,Boolean.TRUE);
+                xlsReport.setParameter(JRXlsExporterParameter.IS_ONE_PAGE_PER_SHEET,Boolean.FALSE);
+                
+                xlsReport.exportReport();
             }
             
         }catch(Exception e){
             System.out.println("Não foi possível gerar o relatório, erro: "+e.getMessage());
-            System.out.println("diretorio para salvar: "+System.getProperty("user.dir")+"\\Desktop");
+            System.out.println("diretorio para salvar: "+System.getProperty("user.home") + "\\Desktop"+report);
         }
     }
 }
